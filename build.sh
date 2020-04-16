2ff81537633f13469d8f89dcae0d26d8ab102c2d
i7-8700 <1281261856@qq.com>
2020-04-16 13:36:49 +0800

    build in centos7

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 mkdir -p build
 cd build
-g++ -std=c++17 ../bench/bench.cpp -I../include -o bench
-g++ -std=c++17 ../demo/demo.cpp -I../include -o demo
-g++ -std=c++17 ../server_demo/server_demo.cpp -I../include -lpthread -o server_demo
+g++ -std=c++11 ../bench/bench.cpp -I../include -o bench
+g++ -std=c++11 ../demo/demo.cpp -I../include -o demo
+g++ -std=c++11 ../server_demo/server_demo.cpp -I../include -lpthread -o server_demo
 
 
