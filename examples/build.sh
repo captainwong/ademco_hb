6789a65970c5778d2e9f7d9d3a18878be566d65d
i7-8700 <1281261856@qq.com>
2020-07-30 15:59:11 +0800

    wtf

--- examples/build.sh
+++ examples/build.sh
@@ -2,11 +2,11 @@
 
 mkdir -p build
 cd build
-opt=`echo " -std=c++11 -O3 –DNDEBUG"`
-g++ $opt ../bench/bench.cpp -I../../include -o bench
-g++ $opt ../demo/demo.cpp -I../../include -o demo
-g++ $opt ../server_demo/server_demo.cpp -I../../include -lpthread -o server_demo
-g++ $opt ../server_demo_libevent/server_demo_libevent.cpp -I../../include `pkg-config --cflags --libs libevent` -levent_core -levent_pthreads -lpthread -o server_demo_libevent
-g++ $opt ../bench_client/bench_client.cpp -I../../include `pkg-config --cflags --libs libevent` -levent_core -levent_pthreads -lpthread -o bench_client
+opt="-std=c++11 -O3"
+g++ $opt –DNDEBUG ../bench/bench.cpp -I../../include -o bench
+g++ $opt –DNDEBUG ../demo/demo.cpp -I../../include -o demo
+g++ $opt –DNDEBUG ../server_demo/server_demo.cpp -I../../include -lpthread -o server_demo
+g++ $opt –DNDEBUG ../server_demo_libevent/server_demo_libevent.cpp -I../../include `pkg-config --cflags --libs libevent` -levent_core -levent_pthreads -lpthread -o server_demo_libevent
+g++ $opt –DNDEBUG ../bench_client/bench_client.cpp -I../../include `pkg-config --cflags --libs libevent` -levent_core -levent_pthreads -lpthread -o bench_client
 
 
