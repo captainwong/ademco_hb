a197448ce3eb12b500c4a24b952d3693ba07067a
i7-8700 <1281261856@qq.com>
2020-08-04 23:43:39 +0800

    update

--- examples/build.sh
+++ examples/build.sh
@@ -2,11 +2,11 @@
 
 mkdir -p build
 cd build
-opt="-std=c++11 -O3 -g -gdwarf"
+opt="-std=c++11 -O3"
 g++ $opt ../bench/bench.cpp -I../../include -o bench
 g++ $opt ../demo/demo.cpp -I../../include -o demo
 g++ $opt ../server_demo/server_demo.cpp -I../../include -lpthread -o server_demo
-g++ -std=c++11 -O3 -DNDEBUG ../server_demo_libevent/server_demo_libevent.cpp -I../../include `pkg-config --cflags --libs libevent` -levent_core -levent_pthreads -lpthread -o server_demo_libevent
+g++ -std=c++11 -O3 -DNDEBUG -g -gdwarf ../server_demo_libevent/server_demo_libevent.cpp -I../../include `pkg-config --cflags --libs libevent breakpad` -levent_core -levent_pthreads -lpthread -o server_demo_libevent
 g++ $opt ../bench_client/bench_client.cpp -I../../include `pkg-config --cflags --libs libevent` -levent_core -levent_pthreads -lpthread -o bench_client
 
 
