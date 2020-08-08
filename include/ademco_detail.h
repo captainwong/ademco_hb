28e4ee620b23d92a706bf5513151e1c04c4c9c0e
i7-8700 <1281261856@qq.com>
2020-08-08 23:31:39 +0800

    fix warning

--- include/ademco_detail.h
+++ include/ademco_detail.h
@@ -11,13 +11,13 @@
 #ifndef JLIB_DISABLE_LOG
 #include <jlib/log2.h>
 #else // JLIB_DISABLE_LOG
-#define init_logger
-#define JLOG_DBUG
-#define JLOG_INFO
-#define JLOG_WARN
-#define JLOG_ERRO
-#define JLOG_CRTC
-#define JLOG_ALL
+#define init_logger(...)
+#define JLOG_DBUG(...)
+#define JLOG_INFO(...)
+#define JLOG_WARN(...)
+#define JLOG_ERRO(...)
+#define JLOG_CRTC(...)
+#define JLOG_ALL(...)
 
 class range_log {
 public:
@@ -27,10 +27,10 @@ public:
 
 #define AUTO_LOG_FUNCTION
 
-#define dump_hex
-#define dump_asc
-#define JLOG_HEX
-#define JLOG_ASC
+#define dump_hex(...)
+#define dump_asc(...)
+#define JLOG_HEX(...)
+#define JLOG_ASC(...)
 #endif // JLIB_DISABLE_LOG
 
 namespace ademco
