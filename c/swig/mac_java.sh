69935fb127d2168b4c27b6fd4b3b4b06c397e750
i7-8700 <1281261856@qq.com>
2022-07-30 17:02:49 +0800

    testing mac

--- c/swig/mac_java.sh
+++ c/swig/mac_java.sh
@@ -2,8 +2,8 @@
 
 mkdir -p com/hb3344/ademco
 swig -java -package com.hb3344.ademco -outdir com/hb3344/ademco -o libademco_java_wrap.c libademco.swig
-JAVA_INCLUDE=/usr/lib/jvm/java-8-openjdk-amd64/include
-JAVA_INCLUDE_OS=${JAVA_INCLUDE}/linux
+JAVA_INCLUDE=/Library/Java/JavaVirtualMachines/jdk-9.0.1.jdk/Contents/Home/include
+JAVA_INCLUDE_OS=${JAVA_INCLUDE}/darwin
 cc -fpic -c ../ademco.c libademco_java_wrap.c -I"../" -I${JAVA_INCLUDE} -I${JAVA_INCLUDE_OS}
 ld -shared ademco.o libademco_java_wrap.o -o libademco.so
 javac JavaDemo.java
