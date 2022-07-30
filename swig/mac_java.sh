163c79b6e8f86a0e74703146c4828631c5a49636
i7-8700 <1281261856@qq.com>
2022-07-31 02:43:35 +0800

    wtf

--- swig/mac_java.sh
+++ swig/mac_java.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p com/hb3344/ademco
-swig -java -package com.hb3344.ademco -outdir com/hb3344/ademco -o libademco_java_wrap.c libademco.swig
+/usr/local/bin/swig -java -package com.hb3344.ademco -outdir com/hb3344/ademco -o libademco_java_wrap.c libademco.swig
 JAVA_INCLUDE=${JAVA_HOME}/include
 JAVA_INCLUDE_OS=${JAVA_INCLUDE}/darwin
 gcc -fpic -c ../ademco.c libademco_java_wrap.c -I"../" -I${JAVA_INCLUDE} -I${JAVA_INCLUDE_OS}
