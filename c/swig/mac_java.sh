2acf1fba1ae8ae1e8af7f6efb9b26430324da01b
mbp <1281261856@qq.com>
2022-07-30 17:15:49 +0800

    fix mac

--- c/swig/mac_java.sh
+++ c/swig/mac_java.sh
@@ -4,7 +4,7 @@ mkdir -p com/hb3344/ademco
 swig -java -package com.hb3344.ademco -outdir com/hb3344/ademco -o libademco_java_wrap.c libademco.swig
 JAVA_INCLUDE=/Library/Java/JavaVirtualMachines/jdk-9.0.1.jdk/Contents/Home/include
 JAVA_INCLUDE_OS=${JAVA_INCLUDE}/darwin
-cc -fpic -c ../ademco.c libademco_java_wrap.c -I"../" -I${JAVA_INCLUDE} -I${JAVA_INCLUDE_OS}
-ld -shared ademco.o libademco_java_wrap.o -o libademco.so
+gcc -fpic -c ../ademco.c libademco_java_wrap.c -I"../" -I${JAVA_INCLUDE} -I${JAVA_INCLUDE_OS}
+gcc -shared ademco.o libademco_java_wrap.o -o libademco.so
 javac JavaDemo.java
 java -Djava.library.path=./ JavaDemo
