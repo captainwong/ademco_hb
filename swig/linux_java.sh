e156a4598dcfe83878bdea9c9ac3037e3fb9a743
i7-8700 <1281261856@qq.com>
2023-05-16 18:32:04 +0800

    use c99

--- swig/linux_java.sh
+++ swig/linux_java.sh
@@ -4,7 +4,7 @@ mkdir -p com/hb3344/ademco
 swig -java -package com.hb3344.ademco -outdir com/hb3344/ademco -o libademco_java_wrap.c libademco.swig
 JAVA_INCLUDE=${JAVA_HOME}/include
 JAVA_INCLUDE_OS=${JAVA_INCLUDE}/linux
-cc -fpic -c ../ademco.c ../hb_com.c libademco_java_wrap.c -I"../" -I${JAVA_INCLUDE} -I${JAVA_INCLUDE_OS}
+cc -std=c99 -fpic -c ../ademco.c ../hb_com.c libademco_java_wrap.c -I"../" -I${JAVA_INCLUDE} -I${JAVA_INCLUDE_OS}
 ld -shared ademco.o hb_com.o libademco_java_wrap.o -o libademco_java.so
 javac JavaDemo.java
 # java -Djava.library.path=./ JavaDemo
