b3777053d858f3fef509e1596086f956344e3719
i7-8700 <1281261856@qq.com>
2023-10-19 18:16:38 +0800

    call vcvars64

--- swig/win_node.bat
+++ swig/win_node.bat
@@ -1,3 +1,4 @@
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 C:\Users\Jack\Downloads\swigwin-4.1.1\swig.exe -c++ -javascript -node libademco.swig
 node-gyp configure build
 rem node nodejs-demo.js
