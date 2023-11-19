6fe6ca3eef62620842c5be4240b0996e5dd46f2c
i7-8700 <1281261856@qq.com>
2023-11-19 22:21:00 +0800

    fix mac node

--- tools/mk_macos_node.sh
+++ tools/mk_macos_node.sh
@@ -6,7 +6,7 @@ CURRENT_DIR=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd $CURRENT_DIR
 
 ssh -t ${MACOS_TARGET} <<EOF
-  cd ${LINUX_PROJECT_PATH}
+  cd ${MACOS_PROJECT_PATH}
   git reset --hard HEAD
   git pull
   git checkout ${PROJECT_BRANCH} 
@@ -18,5 +18,5 @@ ssh -t ${MACOS_TARGET} <<EOF
   ./linux_mac_node.sh mac_node
 EOF
 
-scp ${MACOS_TARGET}:${LINUX_PROJECT_PATH}/tools/mac_node.zip ./dist/mac_node.zip
-ssh -t ${MACOS_TARGET} "rm -f ${LINUX_PROJECT_PATH}/tools/mac_node.zip"
+scp ${MACOS_TARGET}:${MACOS_PROJECT_PATH}/tools/mac_node.zip ./dist/mac_node.zip
+ssh -t ${MACOS_TARGET} "rm -f ${MACOS_PROJECT_PATH}/tools/mac_node.zip"
