88437769c59f41bad52d547dcda517afae0baca4
i9-9900kf <1281261856@qq.com>
2025-04-13 17:13:26 +0800

    3.4.10 ex

--- tools/mk_examples.sh
+++ tools/mk_examples.sh
@@ -10,6 +10,7 @@ cd $CURRENT_DIR
 ./examples.bat
 mkdir -p dist/examples/x86
 cp "/h/dev_hb/AlarmCenterTestTools/Release/直连型主机模拟器V1.20.exe" dist/examples/x86/
+cp "/h/dev_hb/AlarmCenterTestTools/Release/AdemcoParser.exe" dist/examples/x86/
 cp "/g/dev_libs/curl-7.87.0/builds/libcurl-vc14-x86-release-dll-ipv6-sspi-schannel/bin/libcurl.dll" dist/examples/x86/
 cp ../examples/Win32/Release/server_demo.exe dist/examples/x86/
 cp ../examples/Release/httprelay.exe dist/examples/x86/
